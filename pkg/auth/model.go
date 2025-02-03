e306e99d600488b6c9e07a6faf22728b8e4efddc
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-02-03 15:22:13 +0700

    fix(auth): correct validation logic for email/username for AuthInput

--- pkg/auth/model.go
+++ pkg/auth/model.go
@@ -20,7 +20,7 @@ func (input *AuthInput) validate(isRegistering bool, v *util.Validator) map[stri
 		isValidEmail := validateEmail(input.Email)
 		v.Check(isValidEmail, "Email", "Email must be valid")
 	} else {
-		v.Check(input.Email != "" && input.Username != "", "Email or Username", "Must not be empty")
+		v.Check(input.Email != "" || input.Username != "", "Email or Username", "Must not be empty")
 	}
 
 	v.Check(input.Password != "", "Password", "Must not be empty")
